075ee30b41634ac0acfabbd833a18618d3a0a7af
sshim <mechurak@gmail.com>
2022-10-31 23:19:30 +0900

    Introduce Firebase (#5)

--- lib/main.dart
+++ lib/main.dart
@@ -11,24 +11,25 @@ import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
+import 'firebase_options.dart';
 import 'models/app_config.dart';
 import 'models/chapter.dart';
 import 'models/subject.dart';
 import 'models/word.dart';
 import 'pages/game_page.dart';
 import 'pages/home_page.dart';
-import 'pages/lecture_page.dart';
 import 'services/firebase_service.dart';
 import 'services/http_service.dart';
 import 'themes/ellas_notes_theme_data.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // TODO: Introduce Firebase
-  // await Firebase.initializeApp();
-  // GetIt.instance.registerSingleton<FirebaseService>(
-  //   FirebaseService(),
-  // );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  GetIt.instance.registerSingleton<FirebaseService>(
+    FirebaseService(),
+  );
   await loadConfig();
   registerHttpService();
   await Hive.initFlutter("hive_boxes"); // Hive 파일들 저장할 경로 지정
