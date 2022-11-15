48c27bf59f30e739a9bcf4f927bfad0fabeaee88
sshim <mechurak@gmail.com>
2022-11-16 00:06:58 +0900

    Use default font instead of Google Fonts (#10)

--- lib/themes/custom_text_style.dart
+++ lib/themes/custom_text_style.dart
@@ -1,10 +1,12 @@
 import 'package:ellas_notes_flutter/themes/color_styles.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class CustomTextStyle {
   static TextStyle hint(BuildContext context) {
-    return GoogleFonts.nanumBrushScript(textStyle: Theme.of(context).textTheme.titleLarge);
+    return Theme.of(context).textTheme.titleMedium!.copyWith(
+      color: ColorStyles.darkGray,
+    );
+    // return GoogleFonts.nanumBrushScript(textStyle: Theme.of(context).textTheme.titleLarge);
   }
 
   static TextStyle memo(BuildContext context) {
--- lib/themes/ellas_notes_theme_data.dart
+++ lib/themes/ellas_notes_theme_data.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class EllasNotesThemeData {
   static ThemeData lightThemeData = themeData();
 
   static ThemeData themeData() {
     final base = ThemeData.light();
-    return base.copyWith(
-      textTheme: _buildEllasNotesTextTheme(base.textTheme),
-    );
+    return base;
+    // TODO: Add fonts
+    // return base.copyWith(
+    //   textTheme: _buildEllasNotesTextTheme(base.textTheme),
+    // );
   }
 
   static TextTheme _buildEllasNotesTextTheme(TextTheme base) {
     return base.copyWith(
-      titleLarge: GoogleFonts.robotoSlab(textStyle: base.titleLarge), // main text
-      bodyMedium: GoogleFonts.nanumGothic(textStyle: base.bodyMedium), // note
+      // titleLarge: GoogleFonts.robotoSlab(textStyle: base.titleLarge), // main text
+      // bodyMedium: GoogleFonts.nanumGothic(textStyle: base.bodyMedium), // note
     );
   }
 }
